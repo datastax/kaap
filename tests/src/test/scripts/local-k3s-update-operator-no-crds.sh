57f7babc26ec13878c068e5775e190895689e527
Nicolò Boschi <boschi1997@gmail.com>
2023-06-28 10:44:29 +0200

    Rename docker image to datastax/k8saap (#92)

--- tests/src/test/scripts/local-k3s-update-operator-no-crds.sh
+++ tests/src/test/scripts/local-k3s-update-operator-no-crds.sh
@@ -35,7 +35,7 @@ echo "copying image into container $container"
 docker inspect k8saap-local-k3s-network  | jq -r '.[0].Containers[].Name' | while read container; do
   docker cp $tmp_dir/operator.bin $container:/tmp/operator.bin
   echo "image digest copied into container $container"
-  docker exec -t $container sh -c "(ctr image rm docker.io/datastax/lunastreaming-operator:latest-dev) || (docker image rm -f docker.io/datastax/lunastreaming-operator:latest-dev) || echo 'not able to remove'"
+  docker exec -t $container sh -c "(ctr image rm docker.io/datastax/k8saap:latest-dev) || (docker image rm -f docker.io/datastax/k8saap:latest-dev) || echo 'not able to remove'"
   echo "importing image in $container"
   docker exec -t $container sh -c "(ctr image import /tmp/operator.bin) || (docker image load /tmp/operator.bin) || echo 'not able to import'"
   echo "image imported in $container"

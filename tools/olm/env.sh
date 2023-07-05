ae77af4cabf3790a6e4499c2c0a13918371e4365
Nicolò Boschi <boschi1997@gmail.com>
2023-07-05 14:25:37 +0200

    license

--- tools/olm/env.sh
+++ tools/olm/env.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-################################################################################
-#  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Copyright DataStax, Inc.
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
-################################################################################
+#
+
 #
 # Change this file to your need
 
@@ -29,9 +28,9 @@ BUNDLE_VERSION=0.1.0
 # NOTE: If you specify a public registry such as docker.io, you need to login first.
 # i.e. docker login docker.io -u your_docker_org
 DOCKER_REGISTRY="docker.io"
-DOCKER_ORG="nicoloboschi"
-IMAGE_TAG="10m"
+DOCKER_ORG="change-me-to-your-org"
+IMAGE_TAG="olm-gen1"
 
 # Specify the operator image which is used in this bundle.
 # ie.: OPERATOR_IMG="docker.io/datastax/kaap:${BUNDLE_VERSION}"
-OPERATOR_IMG=docker.io/datastax/kaap:latest
+OPERATOR_IMG=docker.io/datastax/kaap:$BUNDLE_VERSION
